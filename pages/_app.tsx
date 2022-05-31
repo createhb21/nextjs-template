355be2896bafba26494078f859fe71fe7b37c6f9
createhb21 <alsghk9701@gmail.com>
2022-05-31 10:44:18 +0900

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,39 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import Head from 'next/head';
+import { RecoilRoot } from 'recoil';
+import type { AppProps } from 'next/app';
+import { Global, ThemeProvider } from '@emotion/react';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { GlobalStyle, theme } from 'styles';
+import { SEO, LayoutWrapper } from 'components';
+
+// React Query의 QueryClient 설정. 나중에 추가할 예정입니다.
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {},
+  },
+});
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <RecoilRoot>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={theme}>
+          <Head>
+            <meta
+              content="width=device-width, initial-scale=1"
+              name="viewport"
+            />
+          </Head>
+          <SEO />
+          <Global styles={GlobalStyle} />
+          <LayoutWrapper>
+            <Component {...pageProps} />
+          </LayoutWrapper>
+        </ThemeProvider>
+      </QueryClientProvider>
+    </RecoilRoot>
+  );
 }
 
-export default MyApp
+export default App;
